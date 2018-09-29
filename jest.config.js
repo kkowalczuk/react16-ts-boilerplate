2e5e913fecbd0bfb516e392dabb56508571e49d7
Karol Kowalczuk <karolkowalczuk1992@gmail.com>
2018-09-29 15:10:53 +0200

    Add code coverage checking by default

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupTestFrameworkScriptFile: "<rootDir>/test.config.ts",
-  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!**/node_modules/**"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/index.tsx", "!**/node_modules/**"],
   snapshotSerializers: ["enzyme-to-json/serializer"]
 };
